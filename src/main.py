7c1895839e7f44bd8486ed0763f71804351e6c65
Daniil <denbuchinskiy@gmail.com>
2026-05-16 13:09:21 +0500

    refactor: create final main application template

--- src/main.py
+++ src/main.py
@@ -124,3 +124,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+from user_menu import show_menu, run_choice
+
+show_menu()
+choice = input("Введите номер действия: ")
+run_choice(choice)
